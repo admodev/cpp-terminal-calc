f6cd190dcbe11d009cbf417f0e00d5e6c28ce0d9
admodev <admosdigital@yahoo.com>
2020-10-08 15:17:30 -0300

    init project, simple cpp terminal calculator

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "TermCalc" << endl << endl;
+    cout << "Por favor seleccione la operacion a realizar. Formato: a+b | a-b | a*b | a/b"
+        << endl;
+    return 0;
+}
